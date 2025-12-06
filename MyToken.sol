b4f7c7c4ff5e868debed63cd2e32d7a0f764bf8c
sahithi9755 <sahithi15072006@gmail.com>
2025-12-06 13:45:35 +0530

    Added ERC-20 Token project files

---
+++ MyToken.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract MyToken {
+
+    string public name = "My Token";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 _totalSupply) {
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(to != address(0), "Cannot transfer to zero address");
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool success) {
+        require(spender != address(0), "Cannot approve zero address");
+
+        allowance[msg.sender][spender] = value;
+
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool success) {
+        require(to != address(0), "Cannot transfer to zero address");
+        require(balanceOf[from] >= value, "Insufficient balance");
+        require(allowance[from][msg.sender] >= value, "Insufficient allowance");
+
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    // Helper functions
+    function getTotalSupply() public view returns (uint256) {
+        return totalSupply;
+    }
+
+    function getTokenInfo()
+        public
+        view
+        returns (string memory, string memory, uint8, uint256)
+    {
+        return (name, symbol, decimals, totalSupply);
+    }
+}
